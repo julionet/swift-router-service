673206eff3d9c18048b7018f11b51f523f0f4ead
Júlio <julio_net@msn.com>
2026-09-16 16:27:10 -0300

    Add InfoContract.swift for API request/response

---
+++ Example/SwiftApp/Features/Info/InfoContract.swift
@@ -0,0 +1,16 @@
+//
+//  InfoContract.swift
+//  SwiftApp
+//
+//  Created by Jose Julio Junior on 15/09/26.
+//
+
+import Foundation
+
+struct InfoRequest {
+    // TODO: adicione aqui os campos enviados para a API.
+}
+
+struct InfoResponse: Decodable {
+    // TODO: adicione aqui os campos retornados pela API.
+}
