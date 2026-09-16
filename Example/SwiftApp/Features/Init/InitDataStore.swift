269acafffac56221629bdc1ec5b060cd9263344d
Júlio <julio_net@msn.com>
2026-09-16 16:23:07 -0300

    Create InitDataStore struct for data handling

---
+++ Example/SwiftApp/Features/Init/InitDataStore.swift
@@ -0,0 +1,14 @@
+//
+//  InitDataStore.swift
+//  SwiftApp
+//
+//  Created by Jose Julio Junior on 15/09/26.
+//
+
+import Foundation
+
+struct InitDataStore {
+    // TODO: adicione aqui os dados que esta cena recebe da cena anterior.
+
+    init() {}
+}
