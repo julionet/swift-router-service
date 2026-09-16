baaed3877ec170fb08eff1cb5603a6b0be67991f
Júlio <julio_net@msn.com>
2026-09-16 16:26:11 -0300

    Add InfoModel and SceneModel structures
    Created InfoModel struct with nested SceneModel.

---
+++ Example/SwiftApp/Features/Info/InfoModel.swift
@@ -0,0 +1,15 @@
+//
+//  InfoModel.swift
+//  SwiftApp
+//
+//  Created by Jose Julio Junior on 15/09/26.
+//
+
+import Foundation
+
+struct InfoModel {
+    struct SceneModel {
+        // TODO: adicione aqui os campos necessários para exibir a tela.
+        var title: String?
+    }
+}
