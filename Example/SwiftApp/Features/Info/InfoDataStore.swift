d574e28684da92763e70edbcc8b9d3c2fd6068d1
Júlio <julio_net@msn.com>
2026-09-16 16:26:54 -0300

    Create InfoDataStore struct for data management

---
+++ Example/SwiftApp/Features/Info/InfoDataStore.swift
@@ -0,0 +1,14 @@
+//
+//  InfoDataStore.swift
+//  SwiftApp
+//
+//  Created by Jose Julio Junior on 15/09/26.
+//
+
+import Foundation
+
+struct InfoDataStore {
+    // TODO: adicione aqui os dados que esta cena recebe da cena anterior.
+
+    init() {}
+}
