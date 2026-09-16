ef81613dabb5955a88169d1bc3dd7b91708a8ddf
Júlio <julio_net@msn.com>
2026-09-16 16:15:11 -0300

    Add AppRouterService for routing functionality

---
+++ Example/SwiftApp/Support/AppRouterService.swift
@@ -0,0 +1,12 @@
+//
+//  AppRouterService.swift
+//  SwiftApp
+//
+//  Created by Jose Julio Junior on 15/09/26.
+//
+
+import SwiftRouterService
+
+public final class AppRouterService {
+    public static let shared = RouterService()
+}
