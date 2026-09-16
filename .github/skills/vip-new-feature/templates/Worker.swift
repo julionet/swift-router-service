d36b728fe10cbb4535b95d55718f264e9013ce07
Júlio <julio_net@msn.com>
2026-09-16 18:23:10 -0300

    Update Worker.swift template for new feature

---
+++ .github/skills/vip-new-feature/templates/Worker.swift
@@ -0,0 +1,27 @@
+//
+//  {Feature}Worker.swift
+//  App
+//
+//  Created by {Author} on {Date}.
+//
+
+import PromiseKit
+
+protocol {Feature}WorkerLogic {
+    // TODO: declare aqui os métodos de acesso a dados/API
+    // Caso a pasta Providers tenha sido criada, exemplo:
+    // func request() -> Promise<Void>
+}
+
+final class {Feature}Worker: {Feature}WorkerLogic {
+    
+    // MARK: - Worker Logic
+    
+    // TODO: implemente aqui os métodos declarados em {Feature}WorkerLogic
+    // Caso a pasta Providers tenha sido criada, exemplo de implementação:
+    // func request() -> Promise<Void> {
+    //     let provider = {Feature}Provider()
+    //     let transactionProvider: TransactionProvider<Void> = TransactionProvider(provider: provider)
+    //     return transactionProvider.getTransactionPromise()
+    // }
+}
