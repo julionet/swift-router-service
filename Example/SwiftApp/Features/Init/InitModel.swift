88b6879bb98cdc100d21aaa1a8016cd9bb990e47
Júlio <julio_net@msn.com>
2026-09-16 16:22:19 -0300

    Add InitModel with SceneModel structure
    Created InitModel.swift with SceneModel struct.

---
+++ Example/SwiftApp/Features/Init/InitModel.swift
@@ -0,0 +1,16 @@
+//
+//  InitModel.swift
+//  SwiftApp
+//
+//  Created by Jose Julio Junior on 15/09/26.
+//
+
+import Foundation
+
+struct InitModel {
+    struct SceneModel {
+        // TODO: adicione aqui os campos necessários para exibir a tela.
+        var title: String?
+        var buttonText: String?
+    }
+}
