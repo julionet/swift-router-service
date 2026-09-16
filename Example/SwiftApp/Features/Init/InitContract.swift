6a733663e12caa8188ad50c91cb6ec6de4e2c3bc
Júlio <julio_net@msn.com>
2026-09-16 16:23:25 -0300

    Create InitContract.swift for API request/response
    Define InitRequest and InitResponse structures for API interaction.

---
+++ Example/SwiftApp/Features/Init/InitContract.swift
@@ -0,0 +1,16 @@
+//
+//  InitContract.swift
+//  SwiftApp
+//
+//  Created by Jose Julio Junior on 15/09/26.
+//
+
+import Foundation
+
+struct InitRequest {
+    // TODO: adicione aqui os campos enviados para a API.
+}
+
+struct InitResponse: Decodable {
+    // TODO: adicione aqui os campos retornados pela API.
+}
