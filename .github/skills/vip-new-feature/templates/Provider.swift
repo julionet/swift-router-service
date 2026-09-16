ebd39236c1ea1cc4823bc29bf96cf67cd2bdd35f
Júlio <julio_net@msn.com>
2026-09-16 18:21:19 -0300

    Update Provider.swift template for new feature

---
+++ .github/skills/vip-new-feature/templates/Provider.swift
@@ -0,0 +1,15 @@
+//
+//  {Feature}Provider.swift
+//  App
+//
+//  Created by {Author} on {Date}.
+//
+
+import Foundation
+
+final class {Feature}Provider: APIRequestProvider {
+    var httpMethod: RequestHTTPMethod { .get }
+    var path: String { "" }
+    
+    // TODO: adicione headers, parâmetros e demais configurações da requisição
+}
