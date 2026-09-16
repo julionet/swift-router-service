253f2d467aea8780c69ebce6e2535be7b882a109
Júlio <julio_net@msn.com>
2026-09-16 18:05:27 -0300

    Create Keys.swift for {Feature} localization
    Added a new Keys.swift file for {Feature} with localization support.

---
+++ .github/skills/mvvm-router-new-feature/templates/Keys.swift
@@ -0,0 +1,19 @@
+//
+//  {Feature}Keys.swift
+//  PanPaymentGateway
+//
+//  Created by {Author} on {Date}.
+//
+
+import Foundation
+import PanExtensions
+
+enum {Feature}Keys {
+
+    enum Localized: String, Localizable {
+        case defaultTitle
+        // TODO: adicionar as demais chaves usadas em {Feature}.strings
+
+        var tableName: String { "{Feature}" }
+    }
+}
